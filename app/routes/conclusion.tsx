96ff1b88bae14f007ba74b7b4c50bbc3ad6d9143
Alex Anderson <hey@ralexanderson.com>
2024-11-01 15:47:43 -0600

    Add the conclusion QR code.

--- app/routes/conclusion.tsx
+++ app/routes/conclusion.tsx
@@ -2,7 +2,12 @@ import { SpeakerNotes } from "~/components/SpeakerNotes";
 
 export default function Thorium() {
   return (
-    <div className="flex h-screen items-center justify-center gap-24">
+    <div className="flex h-screen items-center justify-center gap-4 flex-col">
+      <img src="/assets/qr.svg" className="w-[600px]" alt="qr" />
+      <p className="font-mono text-2xl">https://weird-web.ralexanderson.com</p>
+      <p className="font-mono text-2xl">
+        https://github.com/alexanderson1993/weird-web
+      </p>
       <SpeakerNotes>So. Build weird stuff. The End.</SpeakerNotes>
     </div>
   );
